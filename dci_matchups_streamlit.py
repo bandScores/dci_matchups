8e0a291130527be53b584ba4398223c5179d4bb0
bandScores <marchingartsbtn@gmail.com>
2024-07-28 19:05:57 -0400

    Update dci_matchups_streamlit.py

--- dci_matchups_streamlit.py
+++ dci_matchups_streamlit.py
@@ -14,18 +14,11 @@ st.set_page_config(layout="wide")
 #             unsafe_allow_html=True
 #         )
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+hide_github_icon = """
+
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 data = pd.read_csv('dci_historic.csv')
 max_year = str(data['Year'].iloc[0])
