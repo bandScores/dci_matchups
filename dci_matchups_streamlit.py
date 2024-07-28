db60f85b563ee277f756cd0f10acf8e81ec9c996
bandScores <marchingartsbtn@gmail.com>
2024-07-28 19:02:53 -0400

    Update dci_matchups_streamlit.py

--- dci_matchups_streamlit.py
+++ dci_matchups_streamlit.py
@@ -2,17 +2,30 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(layout="wide")
+# st.markdown(
+#             """
+#             <style>
+#             [data-testid="stElementToolbar"] {
+#                 display: none;
+#             },
+#             footer {visibility: hidden;}
+#             </style>
+#             """,
+#             unsafe_allow_html=True
+#         )
+
 st.markdown(
-            """
-            <style>
-            [data-testid="stElementToolbar"] {
-                display: none;
-            },
-            footer {visibility: hidden;}
-            </style>
-            """,
-            unsafe_allow_html=True
-        )
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 data = pd.read_csv('dci_historic.csv')
 max_year = str(data['Year'].iloc[0])
